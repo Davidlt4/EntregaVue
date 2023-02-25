5238e334505f4358996c1e80a4965fd0240e7b39
David Tapias <99647904+Davidlt4@users.noreply.github.com>
2023-02-25 16:18:10 +0100

    crear curso iniciado

---
+++ src/components/crearCurso.vue
@@ -0,0 +1,76 @@
+<script setup>
+  
+    import { useFirestore,useCollection } from 'vuefire'
+    import { collection} from "firebase/firestore"; 
+    import { getStorage, ref as refStrg, uploadBytes } from "firebase/storage";
+    import { ref } from "vue";
+
+    const db = useFirestore()
+    const cursos = useCollection(collection(db, 'Cursos'))
+
+    let file = ref ("");
+
+    function uploadFile(){
+        const storage = getStorage();
+        const storageRef = refStrg(storage, file.value.files[0].name);
+        uploadBytes(storageRef, file.value.files[0]).then((snapshot) => {
+            console.log('Uploaded a blob file');
+        });
+    }
+    
+    function nuevoCurso(){
+
+        let categoria=document.getElementById("categoria").value;
+        let nombre=document.getElementById("nombre").value;
+        let horas=toString(document.getElementById("horas").value);
+        let imagen=file.value.files[0].name;
+
+        const docRef = addDoc(collection(db, "Cursos"), {
+            categoria: categoria,
+            nombre: nombre,
+            horas: horas,
+            imagen: imagen
+        });
+
+    }
+
+    defineProps({
+        categoria: String,
+        nombre: String,
+        horas: String,
+        imagen: String
+    })
+
+</script>
+
+<template>
+
+    <fieldset>
+        
+        <h3>Nueva Curso: </h3><br>
+
+        <label>Categoria: </label>
+
+        <select name="categoria" id="categoria">
+            <option value="programacion">Programación</option>
+            <option value="SOs">Sistemas Operativos</option>
+            <option value="ofimatica">Ofimatica</option>
+        </select>
+        <br><br>
+
+        <label>Nombre: </label>
+        <input type="text" name="nombre" id="nombre" /><br><br>
+
+        <label>Horas: </label>
+        <input type="number" min="20" max="999" name="horas" id="horas" /><br><br>
+
+        <label>Imagen: </label>
+        <input type="file" name="file" id="file" ref="file" @change="uploadFile">
+        <br><br>
+
+        <button @click="nuevoCurso">Crear Noticia</button>
+
+    </fieldset>
+
+
+</template>
